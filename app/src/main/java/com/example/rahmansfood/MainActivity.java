5583cb3aff9f386431a49300792ec788a76b840e
ryzwannj-dev <ryzwannj@gmail.com>
2025-04-09 14:43:28 +0200

    navbar done

---
+++ app/src/main/java/com/example/rahmansfood/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.rahmansfood;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.rahmansfood.fragments.CreateFragment;
+import com.example.rahmansfood.fragments.HomeFragment;
+import com.example.rahmansfood.fragments.SettingsFragment;
+import com.example.rahmansfood.fragments.StatsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new HomeFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.nav_create) {
+                selectedFragment = new CreateFragment();
+            } else if (itemId == R.id.nav_stats) {
+                selectedFragment = new StatsFragment();
+            } else if (itemId == R.id.nav_settings) {
+                selectedFragment = new SettingsFragment();
+            }
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit();
+            }
+
+            return true;
+        });
+    }
+}
